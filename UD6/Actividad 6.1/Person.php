70b11d103e6c17b2207a9f7ec265e001157a6631
Daniel Armada González <a23danielag@ATEIS.LOCAL>
2024-01-29 17:55:41 +0100

    feat(Person):añade clase Person.php

---
+++ UD6/Actividad 6.1/Person.php
@@ -0,0 +1,33 @@
+<?php
+
+class Person
+{
+    public const species = "Homo Sapiens";
+    public String $name;
+    public int $age;
+    public String $occupation;
+
+    /**
+     * @param String $name
+     * @param String $age
+     * @param String $occupation
+     */
+    public function __construct(string $name, int $age, string $occupation)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->occupation = $occupation;
+    }
+    public function introduce()
+    {
+        return "Hello, my name is $this->name";
+    }
+    public function describe_job()
+    {
+        return "I am currently working as a(n) $this->occupation";
+    }
+    public static function greet_extraterrestrials(String $specie)
+    {
+        return "Welcome to Planet Earth $specie!";
+    }
+}
