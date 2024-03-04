0bb07e00f8d145547b28f1b330844502a0947b95
Daniel Armada González <a23danielag@ATEIS.LOCAL>
2024-03-04 19:08:09 +0100

    feat(SPA):ejemplo previo para actividad 7.2

---
+++ UD7/UD7_Ejemplo_SPA-main/js/global.js
@@ -0,0 +1,14 @@
+
+const base_url = "http://localhost:63342/ApuntesPHP/UD7/UD7_Ejemplo_SPA-main/controller/FrontController.php";
+
+window.onload = onceLoaded;
+
+
+function onceLoaded() {
+
+    console.debug("window loaded");
+    document.querySelector('#formLogin').onsubmit = login;
+    
+    getRoles();
+}
+
